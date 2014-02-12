a0c71d3d39175ab0c95b2a0d87ad2632f7f38596
EvanR <evanrinehart@gmail.com>
2014-02-11 22:41:01 -0600

    init

---
+++ Regex.hs
@@ -0,0 +1,62 @@
+module Regex where
+
+data Regex =
+  Literally String |
+  Capture Regex |
+  AnyOf [CharClass] |
+  AnythingBesides [CharClass] |
+  One CharClass |
+  ZeroOrMore Regex |
+  OneOrMore Regex |
+  Optional Regex |
+  LineStart Regex |
+  LineEnd Regex |
+  Sequence [Regex]
+    deriving (Show, Eq)
+  
+data CharClass =
+  C Char |
+  FromTo Char Char |
+  Digit |
+  Whitespace |
+  WordCharacter |
+  AnythingButNewline |
+  Non CharClass
+    deriving (Show, Eq)
+
+parse :: String -> Either String Regex
+parse = Left "dunno"
+
+
+parse :: Parser Regex
+parse = (eof >> return (Literally "")) <|> (many1 node)
+
+node :: Parser Regex
+node = 
+... use expression parser, you will need to define precedence
+rules for recursive descent
+
+{-
+regex language syntax:
+R+
+R*
+R?
+(R)
+^R
+R$
+^R$
+[CCCC-CC-C]
+abc
+RRRR
+.
+\d
+\w
+\s
+\D
+\W
+\S
+\\ \. \* \+ \? \[ \] \( \) \^ \$
+
+special characters
+\ + * ? ( ) [ ] ^ $
+-}
